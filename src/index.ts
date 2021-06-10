1f24db5e585d8a69776e8f95cf0c303916644def
Alexis Tyler <xo@wvvw.me>
2021-06-10 17:55:19 +0930

    refactor: ts conversion

---
+++ src/index.ts
@@ -0,0 +1,149 @@
+import safe from 'safe-regex';
+import define from 'define-property';
+import extend from 'extend-shallow';
+import not from 'regex-not';
+
+// Max regex length
+const MAX_LENGTH = 1024 * 64;
+
+/**
+ * Session cache
+ */
+const cache = {};
+
+interface Options {
+  /**
+    * Memoize generated regex. This can result in dramatic speed improvements
+    * and simplify debugging by adding options and pattern to the regex. It can be
+    * disabled by setting this to `false`.
+    */
+  cache: boolean;
+}
+
+/**
+ * Memoize generated regex. This can result in dramatic speed improvements
+ * and simplify debugging by adding options and pattern to the regex. It can be
+ * disabled by passing setting `options.cache` to false.
+ */
+function memoize(regex, key: string, pattern, options: Options) {
+  define(regex, 'cached', true);
+  define(regex, 'pattern', pattern);
+  define(regex, 'options', options);
+  define(regex, 'key', key);
+  cache[key] = regex;
+}
+
+/**
+ * Create the key to use for memoization. The key is generated
+ * by iterating over the options and concatenating key-value pairs
+ * to the pattern string.
+ */
+function createKey(pattern, options) {
+  if (!options) return pattern;
+  let key = pattern;
+  for (const prop in options) {
+    if (options.hasOwnProperty(prop)) {
+      key += ';' + prop + '=' + String(options[prop]);
+    }
+  }
+  return key;
+}
+
+/**
+ * Create a regular expression from the given `pattern` string.
+ *
+ * @param pattern Pattern can be a string or regular expression.
+ */
+export const toRegex = function toRegex (patterns: string | RegExp, options: Options): RegExp {
+  if (!Array.isArray(patterns)) {
+    return makeRe(patterns, options);
+  }
+  return makeRe(patterns.join('|'), options);
+};
+
+/**
+ * Create a regular expression from the given `pattern` string.
+ *
+ * @param pattern Pattern can be a string or regular expression.
+ */
+export function makeRe(pattern: string | RegExp, options: Options): RegExp {
+  if (pattern instanceof RegExp) {
+    return pattern;
+  }
+
+  if (typeof pattern !== 'string') {
+    throw new TypeError('expected a string');
+  }
+
+  if (pattern.length > MAX_LENGTH) {
+    throw new Error('expected pattern to be less than ' + MAX_LENGTH + ' characters');
+  }
+
+  let key = pattern;
+  // do this before shallow cloning options, it's a lot faster
+  if (!options || (options && options.cache !== false)) {
+    key = createKey(pattern, options);
+
+    if (cache.hasOwnProperty(key)) {
+      return cache[key];
+    }
+  }
+
+  const opts = extend({}, options);
+  if (opts.contains === true) {
+    if (opts.negate === true) {
+      opts.strictNegate = false;
+    } else {
+      opts.strict = false;
+    }
+  }
+
+  if (opts.strict === false) {
+    opts.strictOpen = false;
+    opts.strictClose = false;
+  }
+
+  const open = opts.strictOpen !== false ? '^' : '';
+  const close = opts.strictClose !== false ? '$' : '';
+  let flags = opts.flags || '';
+  let regex;
+
+  if (opts.nocase === true && !/i/.test(flags)) {
+    flags += 'i';
+  }
+
+  try {
+    if (opts.negate || typeof opts.strictNegate === 'boolean') {
+      pattern = not.create(pattern, opts);
+    }
+
+    const str = open + '(?:' + pattern + ')' + close;
+    regex = new RegExp(str, flags);
+
+    if (opts.safe === true && safe(regex) === false) {
+      throw new Error('potentially unsafe regular expression: ' + regex.source);
+    }
+
+  } catch (err) {
+    if (opts.strictErrors === true || opts.safe === true) {
+      err.key = key;
+      err.pattern = pattern;
+      err.originalOptions = options;
+      err.createdOptions = opts;
+      throw err;
+    }
+
+    try {
+      regex = new RegExp('^' + (pattern as string).replace(/(\W)/g, '\\$1') + '$');
+    } catch (err) {
+      regex = /.^/; //<= match nothing
+    }
+  }
+
+  if (opts.cache !== false) {
+    memoize(regex, key, pattern, opts);
+  }
+  return regex;
+}
+
+export default toRegex;
--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 
 require('mocha');
 var assert = require('assert');
-var toRegex = require('./');
+var { toRegex, makeRe } = require('./dist');
 
 describe('to-regex', function() {
   describe('main export', function() {
@@ -143,84 +143,84 @@ describe('to-regex', function() {
 
   describe('.makeRe', function() {
     it('should be a function', function() {
-      assert.equal(typeof toRegex.makeRe, 'function');
+      assert.equal(typeof makeRe, 'function');
     });
 
     it('should create a strict regex from the given string', function() {
-      assert.deepEqual(toRegex.makeRe('foo'), /^(?:foo)$/);
+      assert.deepEqual(makeRe('foo'), /^(?:foo)$/);
     });
 
     it('should return a regex unchanged', function() {
-      assert.deepEqual(toRegex.makeRe(/whatever/), /whatever/);
+      assert.deepEqual(makeRe(/whatever/), /whatever/);
     });
 
     it('should create a loose regex when `options.contains` is true', function() {
-      assert.deepEqual(toRegex.makeRe('foo', {contains: true}), /(?:foo)/);
+      assert.deepEqual(makeRe('foo', {contains: true}), /(?:foo)/);
     });
 
     it('should create a negation regex when `options.negate` is true', function() {
-      assert.deepEqual(toRegex.makeRe('foo', {negate: true}), /^(?:^(?:(?!^(?:foo)$).)+$)$/);
-      assert.equal(toRegex.makeRe('foo', {negate: true}).test('foo'), false);
-      assert.equal(toRegex.makeRe('foo', {negate: true}).test('bar'), true);
-      assert.equal(toRegex.makeRe('foo', {negate: true}).test('foobar'), true);
-      assert.equal(toRegex.makeRe('foo', {negate: true}).test('barfoo'), true);
+      assert.deepEqual(makeRe('foo', {negate: true}), /^(?:^(?:(?!^(?:foo)$).)+$)$/);
+      assert.equal(makeRe('foo', {negate: true}).test('foo'), false);
+      assert.equal(makeRe('foo', {negate: true}).test('bar'), true);
+      assert.equal(makeRe('foo', {negate: true}).test('foobar'), true);
+      assert.equal(makeRe('foo', {negate: true}).test('barfoo'), true);
     });
 
     it('should create a loose negation regex when `options.strictNegate` is false', function() {
-      assert.deepEqual(toRegex.makeRe('foo', {strictNegate: false}), /^(?:^(?:(?!(?:foo)).)+$)$/);
-      assert.equal(toRegex.makeRe('foo', {strictNegate: false}).test('foo'), false);
-      assert.equal(toRegex.makeRe('foo', {strictNegate: false}).test('bar'), true);
-      assert.equal(toRegex.makeRe('foo', {strictNegate: false}).test('foobar'), false);
-      assert.equal(toRegex.makeRe('foo', {strictNegate: false}).test('barfoo'), false);
+      assert.deepEqual(makeRe('foo', {strictNegate: false}), /^(?:^(?:(?!(?:foo)).)+$)$/);
+      assert.equal(makeRe('foo', {strictNegate: false}).test('foo'), false);
+      assert.equal(makeRe('foo', {strictNegate: false}).test('bar'), true);
+      assert.equal(makeRe('foo', {strictNegate: false}).test('foobar'), false);
+      assert.equal(makeRe('foo', {strictNegate: false}).test('barfoo'), false);
     });
 
     it('should create a loose negation regex when `options.contains` and `options.negate` are true', function() {
-      assert.deepEqual(toRegex.makeRe('foo', {contains: true, negate: true}), /^(?:^(?:(?!(?:foo)).)+$)$/);
-      assert.equal(toRegex.makeRe('foo', {contains: true, negate: true}).test('foo'), false);
-      assert.equal(toRegex.makeRe('foo', {contains: true, negate: true}).test('bar'), true);
-      assert.equal(toRegex.makeRe('foo', {contains: true, negate: true}).test('foobar'), false);
-      assert.equal(toRegex.makeRe('foo', {contains: true, negate: true}).test('barfoo'), false);
+      assert.deepEqual(makeRe('foo', {contains: true, negate: true}), /^(?:^(?:(?!(?:foo)).)+$)$/);
+      assert.equal(makeRe('foo', {contains: true, negate: true}).test('foo'), false);
+      assert.equal(makeRe('foo', {contains: true, negate: true}).test('bar'), true);
+      assert.equal(makeRe('foo', {contains: true, negate: true}).test('foobar'), false);
+      assert.equal(makeRe('foo', {contains: true, negate: true}).test('barfoo'), false);
     });
 
     it('should not enforce beginning anchor when `options.strictOpen` is false', function() {
-      assert.deepEqual(toRegex.makeRe('foo', {strictOpen: false}), /(?:foo)$/);
+      assert.deepEqual(makeRe('foo', {strictOpen: false}), /(?:foo)$/);
     });
 
     it('should not enforce ending anchor when `options.strictClose` is false', function() {
-      assert.deepEqual(toRegex.makeRe('foo', {strictClose: false}), /^(?:foo)/);
+      assert.deepEqual(makeRe('foo', {strictClose: false}), /^(?:foo)/);
     });
 
     it('should use flags passed on `options.flags`', function() {
-      assert.deepEqual(toRegex.makeRe('foo', {flags: 'ig'}), /^(?:foo)$/ig);
+      assert.deepEqual(makeRe('foo', {flags: 'ig'}), /^(?:foo)$/ig);
     });
 
     it('should cache regex by default', function() {
-      toRegex.makeRe('whatever');
-      toRegex.makeRe('whatever');
-      toRegex.makeRe('whatever');
-      var re = toRegex.makeRe('whatever');
+      makeRe('whatever');
+      makeRe('whatever');
+      makeRe('whatever');
+      var re = makeRe('whatever');
 
       assert.deepEqual(re, /^(?:whatever)$/);
       assert.equal(re.cached, true);
     });
 
     it('should not cache regex when options.cache is false', function() {
-      toRegex.makeRe('whatever', {cache: false});
-      toRegex.makeRe('whatever', {cache: false});
-      toRegex.makeRe('whatever', {cache: false});
-      var re = toRegex.makeRe('whatever', {cache: false});
+      makeRe('whatever', {cache: false});
+      makeRe('whatever', {cache: false});
+      makeRe('whatever', {cache: false});
+      var re = makeRe('whatever', {cache: false});
 
       assert.deepEqual(re, /^(?:whatever)$/);
       assert.equal(typeof re.cached, 'undefined');
     });
 
     it('should add `i` to flags when `options.nocase` is true', function() {
-      assert.deepEqual(toRegex.makeRe('foo', {nocase: true}), /^(?:foo)$/i);
+      assert.deepEqual(makeRe('foo', {nocase: true}), /^(?:foo)$/i);
     });
 
     it('should throw an error when invalid args are passed', function(cb) {
       try {
-        toRegex.makeRe();
+        makeRe();
         cb(new Error('expected an error'));
       } catch (err) {
         assert(err);
