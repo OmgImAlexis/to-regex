a561cd5699595fdefb40734364885a1ec507462a
Alexis Tyler <xo@wvvw.me>
2021-06-10 18:01:07 +0930

    fix: types

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,33 @@ const cache = {};
 
 interface Options {
   /**
-    * Memoize generated regex. This can result in dramatic speed improvements
-    * and simplify debugging by adding options and pattern to the regex. It can be
-    * disabled by setting this to `false`.
-    */
+   * Generate a regex that will match any string that contains the given pattern.
+   * By default, regex is strict will only return `true` for exact matches.
+   */
+  contains?: boolean;
+  /**
+   * Create a regex that will match everything except the given pattern.
+   */
+  negate?: boolean;
+  /**
+   * Adds the `i` flag, to enable case-insensitive matching.
+   * Alternatively you can pass the flags you want directly with `options.flags`.
+   */
+  nocase?: boolean;
+  /**
+   * Define the flags you want to use on the generated regex.
+   */
+  flags?: string;
+  /**
+   * Generated regex is cached based on the provided string and options.
+   * As a result, runtime compilation only happens once per pattern (as long as options are also the same), which can result in dramatic speed improvements.
+   * This also helps with debugging, since adding options and pattern are added to the generated regex.
+   */
   cache: boolean;
+  /**
+   * Check the generated regular expression with safe-regex and throw an error if the regex is potentially unsafe.
+   */
+  safe: boolean;
 }
 
 /**
@@ -50,11 +72,9 @@ function createKey(pattern, options) {
 }
 
 /**
- * Create a regular expression from the given `pattern` string.
- *
- * @param pattern Pattern can be a string or regular expression.
+ * Create a regular expression from the given `pattern`.
  */
-export const toRegex = function toRegex (patterns: string | RegExp, options: Options): RegExp {
+export const toRegex = function toRegex (patterns: string | string[] | RegExp, options: Options): RegExp {
   if (!Array.isArray(patterns)) {
     return makeRe(patterns, options);
   }
@@ -63,10 +83,8 @@ export const toRegex = function toRegex (patterns: string | RegExp, options: Opt
 
 /**
  * Create a regular expression from the given `pattern` string.
- *
- * @param pattern Pattern can be a string or regular expression.
  */
-export function makeRe(pattern: string | RegExp, options: Options): RegExp {
+export function makeRe(pattern: string | string[] | RegExp, options: Options): RegExp {
   if (pattern instanceof RegExp) {
     return pattern;
   }
